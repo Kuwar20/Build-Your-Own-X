66bb649c4849cf6d38aed0a21af6e60d4856e228
Kuwar20 <kuwarx1@gmail.com>
2024-10-10 21:25:40 +0530

    Extract url path

--- Build HTTP Server From Scratch/server.js
+++ Build HTTP Server From Scratch/server.js
@@ -1,14 +1,18 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-
-    socket.write('HTTP/1.1 200 OK\r\n\r\n');
-    
     socket.on("close", () => {
         socket.end();
     });
+
+    socket.on("data", (data) => {
+        console.log(data);
+        const path = data.toString().split(" ")[1];
+        const responseStatus = path === "/home" ? "200 OK" : "404 Not Found";
+        socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+    });
 });
 
-server.listen(4221, "localhost", ()=>{
+server.listen(4221, "localhost", () => {
     console.log("Server is running on port 4221");
 });
