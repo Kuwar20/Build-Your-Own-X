d705f51818b84cfe02f5e32115ac6d90dad1928e
Kuwar20 <kuwarx1@gmail.com>
2024-10-10 22:04:39 +0530

    make concurrent connection

--- Build HTTP Server From Scratch/server.js
+++ Build HTTP Server From Scratch/server.js
@@ -1,16 +1,16 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-    socket.on("close", () => {
-        socket.end();
-    });
 
     socket.on("data", (data) => {
         const request = data.toString();
         console.log("Request: \n" + request);
 
         const url = request.split(" ")[1];
+        const headers = request.split("\r\n");
+
         
+        // endpoints and their responses
         if (url == "/") {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
         } else if (url.includes("/echo/")) {
@@ -18,10 +18,25 @@ const server = net.createServer((socket) => {
             socket.write(
                 `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
             );
-        } else {
+        }
+        else if (url == "/user-agent") {
+            const userAgent = headers[2].split("User-Agent: ")[1];
+            socket.write(
+                `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
+            );
+        }
+        else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
     });
+
+        //Closing
+        socket.on("close", () => {
+            socket.end();
+            // server.close();  // to make it concurrent connections (in js)
+        });
+
+        
 });
 
 server.listen(4221, "localhost", () => {
